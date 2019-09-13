8ecc7a951f27af34b78e8431289d00549047722e
jacobcalv <jacobcalvino@outlook.com>
2019-09-12 20:40:48 -0500

    finished MVP and did some css styling to make it look good

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,32 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {withFormik, Form, Field} from 'formik';
 import * as yup from 'yup';
 import axios from 'axios';
 import '../App.css'
 
 const UserForm = ({errors, touched, status}) => {
+    console.log(status)
+    const [users, setUser] = useState([])
+
+    useEffect(()=>{
+        if(status) {
+            setUser([...users, status])
+        }
+    }, [status])
     return(
-        <div className='reg'>
-            <h1>Registration</h1>
-            <Form>
-                {touched.name && errors.name && <p className='error'>{errors.name}</p>}
-                <Field type='text' name='name' placeholder='Name'/>
+        <div className='container'>
+            <div className='reg'>
+                <h1>Registration</h1>
+                <Form>
+                    {touched.name && errors.name && <p className='error'>{errors.name}</p>}
+                    <Field type='text' name='name' placeholder='Name'/>
 
-                {touched.email && errors.email && <p className='error'>{errors.email}</p>}
-                <Field type='text' name='email' placeholder='Email'/>
+                    {touched.email && errors.email && <p className='error'>{errors.email}</p>}
+                    <Field type='text' name='email' placeholder='Email'/>
 
-                {touched.password && errors.password && <p className='error'>{errors.password}</p>}
-                <Field type='text' name='password' placeholder='Password'/>
+                    {touched.password && errors.password && <p className='error'>{errors.password}</p>}
+                    <Field type='password' name='password' placeholder='Password'/>
 
-                {touched.terms && errors.terms && <p className='error'>{errors.terms}</p>}
-                <label>
-                    <Field type='checkbox' name='terms'/>
-                    <span>Accept Terms of Service</span>
-                </label>
-                <button type='submit'>Submit</button>
-            </Form>
+                    {touched.terms && errors.terms && <p className='error'>{errors.terms}</p>}
+                    <label>
+                        <Field type='checkbox' name='terms'/>
+                        <span>Accept Terms of Service</span>
+                    </label>
+                    <button type='submit'>Submit</button>
+                </Form>
+            </div>
+            <div className='nameBoat'>
+                <h2 className='title'>Current Users</h2>
+                    {users.map(user => (
+                        <h3 className='listOfNames'>{user.name}</h3>
+                    ))}
+            </div>
         </div>
-        
     )
 }
 
@@ -41,12 +56,20 @@ export default withFormik({
         }
     },
     validationSchema: yup.object().shape({
-        name: yup.string().required('Name Field Must Be Filled Out'),
-        email: yup.string().required('Email Field Must Be Filled Out'),
-        password: yup.string().required('Password Must Be Filled Out'),
-        terms: yup.boolean().oneOf([true], 'You Must Accept Terms of Service')
+        name: yup.string().required('Name Field Must Be Filled Out').min(2| ReferenceError,'✔️ Name must be more than 2 characters'),
+        email: yup.string().required('Email Field Must Be Filled Out').email('✔️ Must Be a Vaild Email'),
+        password: yup.string().required('Password Must Be Filled Out').min(5 | ReferenceError, '✔️ Password Must Be >5'),
+        terms: yup.boolean().oneOf([true], '✔️ You Must Accept Terms of Service')
     }),
-    handleSubmit: (values)=>{
+    handleSubmit: (values, {setStatus})=>{
         console.log(values)
+        axios.post("https://reqres.in/api/users", values)
+            .then((res)=>{
+                console.log(res)
+                setStatus(res.data)
+            })
+            .catch((err)=>{
+                console.log(err)
+            })
     }
 })(UserForm);
